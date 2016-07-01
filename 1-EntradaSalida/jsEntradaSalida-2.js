809478c33b7c45cdbbfb178ec336e1f7c34ed417
sotomayormatias <matisoto_77@hotmail.com>
2016-07-01 20:03:57 -0300

    este es el primer commit que hago

---
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -0,0 +1,8 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var textoIngresado = prompt("Por favor ingrese su nombre", "UTN FRA Ingrese nombre");
+	alert(textoIngresado);
+}
+
