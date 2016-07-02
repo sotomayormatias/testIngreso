9c40a4b306cbdb64c3c6b3d819ee68169c862111
sotomayormatias <matisoto_77@hotmail.com>
2016-07-01 21:56:26 -0300

    Ejercicios 9 y 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,8 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+	var importe = document.getElementById("importe").value;
+	var resultado = parseInt(importe) - parseInt(importe) * 0.25;
+
+	document.getElementById("resultado").value = resultado;
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,8 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+	var sueldo = document.getElementById("sueldo").value;
+	var resultado = parseInt(sueldo) + parseInt(sueldo) * 0.1;
 
-	
+	document.getElementById("resultado").value = resultado;
 }
