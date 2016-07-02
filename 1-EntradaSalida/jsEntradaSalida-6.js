f63a71d0bae59347c0de28ad8cf60e746f343219
sotomayormatias <matisoto_77@hotmail.com>
2016-07-01 21:06:02 -0300

    Ejercicios 6 y 7

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,10 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var numeroUno = document.getElementById("numeroUno").value;
+	var numeroDos = document.getElementById("numeroDos").value;
 
+	resultado = parseInt(numeroUno) + parseInt(numeroDos);
+	alert("la suma es: " + resultado);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,37 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("sumar");
+	var numeroUno = document.getElementById("numeroUno").value;
+	var numeroDos = document.getElementById("numeroDos").value;
+
+	resultado = parseInt(numeroUno) + parseInt(numeroDos);
+	alert("la suma es: " + resultado);
 }
 
 function restar()
 {
-	
+	var numeroUno = document.getElementById("numeroUno").value;
+	var numeroDos = document.getElementById("numeroDos").value;
+
+	resultado = parseInt(numeroUno) - parseInt(numeroDos);
+	alert("la resta es: " + resultado);
 }
 
 function multiplicar()
 { 
-	
+	var numeroUno = document.getElementById("numeroUno").value;
+	var numeroDos = document.getElementById("numeroDos").value;
+
+	resultado = parseInt(numeroUno) * parseInt(numeroDos);
+	alert("la multiplicación es: " + resultado);
 }
 
 function dividir()
 {
-	
+	var numeroUno = document.getElementById("numeroUno").value;
+	var numeroDos = document.getElementById("numeroDos").value;
+
+	resultado = parseInt(numeroUno) / parseInt(numeroDos);
+	alert("la división es: " + resultado);
 }
 
